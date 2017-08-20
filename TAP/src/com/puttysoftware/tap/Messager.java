71ef2baeddf20801bb8914f74dd202cb7c4a5e2b
Eric Ahnell <eric.ahnell@puttysoftware.com>
2017-08-20 12:34:08 -0400

    Remove unused methods and import.

--- TAP/src/com/puttysoftware/tap/Messager.java
+++ TAP/src/com/puttysoftware/tap/Messager.java
@@ -5,8 +5,6 @@ Any questions should be directed to the author via email at: tap@worldwizard.net
  */
 package com.puttysoftware.tap;
 
-import javax.swing.JOptionPane;
-
 public class Messager {
     public static void showMessage(final String msg) {
         final Game game = TAP.getGame();
@@ -16,25 +14,5 @@ public class Messager {
     public static void showErrorMessage(final String msg) {
         final Game game = TAP.getGame();
         game.getGUIManager().updateCommandOutput("ADVENTURE ERROR: " + msg);
-    }
-
-    public static void showTitledDialog(final String msg, final String title) {
-        final Game game = TAP.getGame();
-        JOptionPane.showMessageDialog(game.getOutputFrame(), msg, title,
-                JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    public static void showErrorDialog(final String msg, final String title) {
-        final Game game = TAP.getGame();
-        JOptionPane.showMessageDialog(game.getOutputFrame(), msg, title,
-                JOptionPane.ERROR_MESSAGE);
-    }
-
-    public static int showConfirmDialog(final String prompt,
-            final String title) {
-        final Game game = TAP.getGame();
-        return JOptionPane.showConfirmDialog(game.getOutputFrame(), prompt,
-                title, JOptionPane.YES_NO_OPTION,
-                JOptionPane.INFORMATION_MESSAGE);
     }
 }
