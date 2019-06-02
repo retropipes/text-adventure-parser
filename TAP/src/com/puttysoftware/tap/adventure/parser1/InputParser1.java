4b98074bac2e3d10197409ea422e8477c00da8f2
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-06-01 21:02:08 -0400

    Add a skeleton V2 parser and supporting machinery.

---
+++ TAP/src/com/puttysoftware/tap/adventure/parser1/InputParser1.java
@@ -0,0 +1,26 @@
+/*  TAP: A Text Adventure Parser
+Copyright (C) 2010 Eric Ahnell
+
+Any questions should be directed to the author via email at: tap@worldwizard.net
+ */
+package com.puttysoftware.tap.adventure.parser1;
+
+import java.util.ArrayList;
+
+import com.puttysoftware.tap.adventure.parsers.InputParser;
+
+public class InputParser1 implements InputParser {
+    // Fields
+    // Constructor
+    public InputParser1() {
+    }
+
+    // Methods
+    @Override
+    public void doInitial(final ArrayList<String> data) {
+    }
+
+    @Override
+    public void parseCommand(final String command) {
+    }
+}
--- TAP/src/com/puttysoftware/tap/adventure/parsers/ParserFactory.java
+++ TAP/src/com/puttysoftware/tap/adventure/parsers/ParserFactory.java
@@ -1,25 +1,35 @@
 package com.puttysoftware.tap.adventure.parsers;
 
 import com.puttysoftware.tap.adventure.parser0.InputParser0;
+import com.puttysoftware.tap.adventure.parser1.InputParser1;
 
 public final class ParserFactory {
     // Constants
     public static final int GRAMMAR_0 = 0;
+    public static final int GRAMMAR_1 = 1;
     // Caches
     private static InputParser0 PARSER_0;
+    private static InputParser1 PARSER_1;
 
     private ParserFactory() {
         // Do nothing: static methods only
     }
 
     public static InputParser getParser(final int grammarVersion) {
-        // Create parser 1 if needed
+        // Create parser 0 if needed
         if (ParserFactory.PARSER_0 == null) {
             ParserFactory.PARSER_0 = new InputParser0();
+        }
+        // Create parser 1 if needed
+        if (ParserFactory.PARSER_1 == null) {
+            ParserFactory.PARSER_1 = new InputParser1();
         }
         // Get a parser
         if (grammarVersion == ParserFactory.GRAMMAR_0) {
             return ParserFactory.PARSER_0;
+        }
+        if (grammarVersion == ParserFactory.GRAMMAR_1) {
+            return ParserFactory.PARSER_1;
         }
         // Unknown grammar version
         throw new UnsupportedOperationException("" + grammarVersion);
